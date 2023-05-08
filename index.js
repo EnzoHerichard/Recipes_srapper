d8d939aec43f18f38b01c91b13e2ed469e31bec0
EnzoHerichard <herichardenzo@gmail.com>
2023-05-08 16:28:18 +0200

    reorganize files

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
-const express = require('express');
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
-const mysql = require('mysql');
+const mysql = require("mysql");
+const swaggerJSDoc = require("./swagger.json");
 const port = 3000;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'restaurant'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "restaurant",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Erreur de connexion à la base de données : ', err);
+    console.error("Erreur de connexion à la base de données : ", err);
     return;
   }
-  console.log('Connexion à la base de données établie');
+  console.log("Connexion à la base de données établie");
 });
 
-app.get('/recipes', (req, res) => {
-  connection.query('SELECT * FROM recettes', (error, results, fields) => {
+app.get("/recipes", (req, res) => {
+  connection.query("SELECT * FROM recettes", (error, results, fields) => {
     if (error) {
-      console.error('Erreur lors de la récupération des recettes : ', error);
-      res.status(500).send('Erreur lors de la récupération des recettes');
+      console.error("Erreur lors de la récupération des recettes : ", error);
+      res.status(500).send("Erreur lors de la récupération des recettes");
       return;
     }
     res.send(results);
   });
 });
 
-app.get('/recipes/:id_recette/ingredients', (req, res) => {
-    const id_recette = req.params.id_recette;
-    const query = `
-      SELECT DISTINCT i.nom, titre, description, image
-      FROM ingredients_recettes ir 
-      INNER JOIN ingredients i ON ir.ingredient_id = i.id INNER JOIN recettes r ON ir.recette_id = r.id
-      WHERE ir.recette_id = ${id_recette}
-    `;
-    connection.query(query, (error, results, fields) => {
-      if (error) {
-        console.error('Erreur lors de la récupération des ingrédients : ', error);
-        res.status(500).send('Erreur lors de la récupération des ingrédients');
-        return;
-      }
-      res.send(results);
-    });
-  });
-  app.get('/recipes/:id_recette/details', (req, res) => {
-    const id_recette = req.params.id_recette;
-    const query = `
+app.get("/recipes/:id_recette/details", (req, res) => {
+  const id_recette = req.params.id_recette;
+  const query = `
       SELECT r.titre, r.description, r.image, GROUP_CONCAT(DISTINCT i.nom SEPARATOR '-') AS ingredients
       FROM recettes r
       INNER JOIN ingredients_recettes ir ON r.id = ir.recette_id
@@ -56,39 +41,62 @@ app.get('/recipes/:id_recette/ingredients', (req, res) => {
       WHERE r.id = ${id_recette}
       GROUP BY r.id
     `;
-    connection.query(query, (error, results, fields) => {
-      if (error) {
-        console.error('Erreur lors de la récupération des détails de la recette : ', error);
-        res.status(500).send('Erreur lors de la récupération des détails de la recette');
-        return;
-      }
-      if (results.length === 0) {
-        res.status(404).send(`Recette avec l'identifiant ${id_recette} non trouvée`);
-      } else {
-        res.send(results[0]);
-      }
-    });
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error(
+        "Erreur lors de la récupération des détails de la recette : ",
+        error
+      );
+      res
+        .status(500)
+        .send("Erreur lors de la récupération des détails de la recette");
+      return;
+    }
+    if (results.length === 0) {
+      res
+        .status(404)
+        .send(`Recette avec l'identifiant ${id_recette} non trouvée`);
+    } else {
+      res.send(results[0]);
+    }
   });
+});
 
-
-  app.get('/ingredients/:id_recette', (req, res) => {
-    const id_recette = req.params.id_recette;
-    const query = `
+app.get("/ingredients/:id_recette", (req, res) => {
+  const id_recette = req.params.id_recette;
+  const query = `
       SELECT DISTINCT i.nom 
       FROM ingredients_recettes ir 
       INNER JOIN ingredients i ON ir.ingredient_id = i.id 
       WHERE ir.recette_id = ${id_recette}
     `;
-    connection.query(query, (error, results, fields) => {
-      if (error) {
-        console.error('Erreur lors de la récupération des ingrédients : ', error);
-        res.status(500).send('Erreur lors de la récupération des ingrédients');
-        return;
-      }
-      res.send(results);
-    });
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error("Erreur lors de la récupération des ingrédients : ", error);
+      res.status(500).send("Erreur lors de la récupération des ingrédients");
+      return;
+    }
+    res.send(results);
   });
+});
 
+
+app.get("/recipes/:name", async (req, res) => {
+  const encodedName = req.params.name;
+  const name = decodeURIComponent(encodedName); 
+  const query = 
+        `SELECT *
+        FROM recipes
+        WHERE title = ?
+    ;`
+  try {
+    const [results] = await pool.execute(query, [name]);
+    res.json(results);
+  } catch (error) {
+    throw error;
+  }
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc));
 
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
--- scripts/insertDB.js
+++ scripts/insertDB.js
@@ -1,76 +1,16 @@
 //executablePath: 'C:/Users/heric/chrome-win/chrome-win/chrome.exe'
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const mysql = require('mysql2/promise');
-const MongoClient = require('mongodb').MongoClient;
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const mysql = require("mysql2/promise");
+const MongoClient = require("mongodb").MongoClient;
 
-(async () => {
-  const browser = await puppeteer.launch({
-    executablePath: 'C:/Users/heric/chrome-win/chrome-win/chrome.exe'
-  });
-  const page = await browser.newPage();
-  const url = "https://www.allrecipes.com/recipes/";
-  await page.goto(url);
-
-  const hrefs = await page.$$eval("a[data-tax-levels]", (links) =>
-    links.map((link) => link.href)
-  );
-  const recipes = [];
-  const ingredientsList = [];
-  for (const href of hrefs) {
-    const ingredients = []; // Initialiser la liste des ingrédients pour chaque page
-  
-    if (href && href.startsWith("https://www.allrecipes.com/recipe/")) {
-      await page.goto(href);
-      const h1 = await page.$eval("h1", (el) => el.textContent.trim());
-      const imageSrc = await page.$eval(
-        ".img-placeholder img",
-        (img) => img.src
-      );
-      const subheading = await page.$eval(
-        ".comp.type--dog.article-subheading",
-        (el) => el.textContent.trim()
-      );
-  
-      const ingredientList = await page.$('.mntl-structured-ingredients__list');
-      const ingredientEls = await ingredientList.$$('li');
-  
-      for (const ingredientEl of ingredientEls) {
-        const qtIng = await ingredientEl.$eval('span[data-ingredient-quantity]', el => el.textContent.trim());
-        const unitIng = await ingredientEl.$eval('span[data-ingredient-unit]', el => el.textContent.trim());
-        const nameIng = await ingredientEl.$eval('span[data-ingredient-name]', el => el.textContent.trim());
-  
-        ingredients.push({ qt: qtIng, unit: unitIng, name: nameIng });
-      }
-  
-      recipes.push({ title: h1, img: imageSrc, desc: subheading, ingredients: ingredients });
-  
-      for (const ingredient of ingredients) {
-        const found = ingredientsList.find((i) => i.name === ingredient.name);
-        if (found === undefined) {
-          ingredientsList.push({ name: ingredient.name });
-        }
-      }
-    }
-  }
-  fs.writeFile("recipes.json", JSON.stringify(recipes), (err) => {
-    if (err) throw err;
-    console.log("Le fichier JSON a été créé avec succès!");
-  });
-  fs.writeFile("ingredients.json", JSON.stringify(ingredientsList), (err) => {
-    if (err) throw err;
-    console.log("Le fichier JSON a été créé avec succès!");
-  });
-  await browser.close();
-})();
 // INSERTION DES RECETTES ET INGREDIENTS DANS LA BASE DE DONNEES MYSQL
 (async () => {
-
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'restaurant'
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "restaurant",
   });
   await connection.execute(`CREATE TABLE IF NOT EXISTS recettes (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -91,69 +31,63 @@ const MongoClient = require('mongodb').MongoClient;
     FOREIGN KEY (recette_id) REFERENCES recettes(id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
   )`);
- 
 
-const jsonFilePathRecipes = 'recipes.json';
+  const jsonFilePathRecipes = "recipes.json";
 
-try {
-  const data = await fs.promises.readFile(jsonFilePathRecipes, 'utf8');
-  const recipes = JSON.parse(data);
+  try {
+    const data = await fs.promises.readFile(jsonFilePathRecipes, "utf8");
+    const recipes = JSON.parse(data);
 
-  // Boucle sur les recettes et insérez-les dans la base de données
-  
-  for (const recipe of recipes) {
-    const [existingRecipe] = await connection.execute(
-      'SELECT id FROM recettes WHERE titre = ?',
-      [recipe.title]
+    for (const recipe of recipes) {
+      const [existingRecipe] = await connection.execute(
+        "SELECT id FROM recettes WHERE titre = ?",
+        [recipe.title]
+      );
+      let recetteId;
+      if (existingRecipe.length === 0) {
+        const [result] = await connection.execute(
+          "INSERT INTO recettes (titre, description, image) VALUES (?, ?, ?)",
+          [recipe.title, recipe.desc, recipe.img]
+        );
+        recetteId = result.insertId;
+      } else {
+        recetteId = existingRecipe[0].id;
+      }
+
+      for (const ingredient of recipe.ingredients) {
+        const [existingIngredient] = await connection.execute(
+          "SELECT id FROM ingredients WHERE nom = ?",
+          [ingredient.name]
+        );
+        let ingredientId;
+        if (existingIngredient.length === 0) {
+          const [ingredientResult] = await connection.execute(
+            "INSERT INTO ingredients (nom) VALUES (?)",
+            [ingredient.name]
+          );
+          ingredientId = ingredientResult.insertId;
+        } else {
+          ingredientId = existingIngredient[0].id;
+        }
+
+        await connection.execute(
+          "INSERT INTO ingredients_recettes (recette_id, ingredient_id) VALUES (?, ?)",
+          [recetteId, ingredientId]
+        );
+      }
+    }
+    console.log(
+      `Les recettes et les ingrédients ont été insérés avec succès dans la base de données!`
     );
-    let recetteId;
-    if (existingRecipe.length === 0) { // Si la recette n'existe pas, on l'insère
-      const [result] = await connection.execute(
-        'INSERT INTO recettes (titre, description, image) VALUES (?, ?, ?)',
-        [recipe.title, recipe.desc, recipe.img]
-      );
-      recetteId = result.insertId;
-    } else { // Sinon, on récupère l'ID de la recette existante
-      recetteId = existingRecipe[0].id;
-    }
-
-    // INSERTION DES INGREDIENTS DE LA RECETTE
-    for (const ingredient of recipe.ingredients) {
-      const [existingIngredient] = await connection.execute(
-        'SELECT id FROM ingredients WHERE nom = ?',
-        [ingredient.name]
-      );
-      let ingredientId;
-    if (existingIngredient.length === 0) { // Si l'ingrédient n'existe pas, on l'insère
-      const [ingredientResult] = await connection.execute(
-        'INSERT INTO ingredients (nom) VALUES (?)',
-        [ingredient.name]
-      );
-      ingredientId = ingredientResult.insertId;
-    } else { // Sinon, on récupère l'ID de l'ingrédient existant
-      ingredientId = existingIngredient[0].id;
-    }
-
-      
-
-      // INSERTION DE LA LIAISON ENTRE LA RECETTE ET L'INGREDIENT
-      await connection.execute(
-        'INSERT INTO ingredients_recettes (recette_id, ingredient_id) VALUES (?, ?)',
-        [recetteId, ingredientId]
-      );
-    }
+  } catch (err) {
+    console.error(`Erreur lors de la lecture du fichier JSON: ${err}`);
   }
-  console.log(`Les recettes et les ingrédients ont été insérés avec succès dans la base de données!`);
-} catch (err) {
-  console.error(`Erreur lors de la lecture du fichier JSON: ${err}`);
-} 
 
-await connection.end();
-
+  await connection.end();
 })();
 
 // INSERTION DES RECETTES ET INGREDIENTS DANS LA BASE DE DONNEES MONGODB
-const url = 'mongodb://localhost:27017';
+/* const url = 'mongodb://localhost:27017';
 const dbName = 'restaurant';
 
 (async () => {
@@ -173,7 +107,6 @@ const dbName = 'restaurant';
     const recipes = JSON.parse(data);
 
     for (const recipe of recipes) {
-      // INSERTION DE LA RECETTE
       const existingRecipe = await db.collection('recettes').findOne({ titre: recipe.title });
       let recetteId;
       if (!existingRecipe) { // Si la recette n'existe pas, on l'insère
@@ -183,7 +116,6 @@ const dbName = 'restaurant';
         recetteId = existingRecipe._id;
       }
 
-      // INSERTION DES INGREDIENTS DE LA RECETTE
       for (const ingredient of recipe.ingredients) {
         const existingIngredient = await db.collection('ingredients').findOne({ nom: ingredient.name });
         let ingredientId;
@@ -194,7 +126,6 @@ const dbName = 'restaurant';
           ingredientId = existingIngredient._id;
         }
 
-        // INSERTION DE LA LIAISON ENTRE LA RECETTE ET L'INGREDIENT
         await db.collection('ingredients_recettes').insertOne({ recette_id: recetteId, ingredient_id: ingredientId });
       }
     }
@@ -205,3 +136,4 @@ const dbName = 'restaurant';
 
   await client.close();
 })();
+ */
